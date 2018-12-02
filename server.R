69ee62f80f42214d1f44121dbbf078bddf455d2c
Zhen Jiang <zjiang12@ncsu.edu>
2018-12-02 00:01:56 -0800

    For project 3

--- server.R
+++ server.R
@@ -1,30 +1,26 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
-
+library(tree)
 
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
-  getData <- reactive({
-    newData <- diamonds %>% filter(carat > 1)
+  newData <- diamonds %>% filter(carat > 1)
+  
+  #for the information tab content (tab 1)
+  output$info <- renderUI({
+    h4("This is analysis of a dataset-diamonds from the ggplot2 package, it contains the prices and other attributes of almost 54,000 diamonds.")
   })
   
-#for the information tab content (tab 1)
-output$info <- renderUI({
-    h4("This is analysis of a dataset-diamonds from the ggplot2 package, it contains the prices and other attributes of almost 54,000 diamonds.")
-  }) 
-  
-#include a link
-url <- a("Please find detail informations of this data set by clicking here", href="https://cran.r-project.org/web/packages/ggplot2/ggplot2.pdf")
-output$link <- renderUI({
+  #include a link
+  url <- a("Please find detail informations of this data set by clicking here", href="https://cran.r-project.org/web/packages/ggplot2/ggplot2.pdf")
+  output$link <- renderUI({
     tagList("Detail:", url)
   })
   
-# plot for 2nd tab: a scatter plot
-plot1 <- reactive({
-  newData <- getData()
-    
-  # Render a scatter plot
+  # plot for 2nd tab: a scatter plot
+  plot1 <- reactive({
+    # Render a scatter plot
     if(input$carats){ #1st dynamic UI
       if(input$cuts){ #2nd dynamic UI
         ggplot(newData, aes(x = carat, y = price)) + geom_point(aes(color = factor(cut))) +
@@ -37,12 +33,12 @@ plot1 <- reactive({
     }
   })
   
-output$plot1 <- renderPlot({
+  output$plot1 <- renderPlot({
     plot1()
   })
   
-# plot for 3rd tab: a heatmap
-plot2 <- reactive({
+  # plot for 3rd tab: a heatmap
+  plot2 <- reactive({
     if(input$carats){
       if(input$cuts){
         ggplot(newData, aes(carat, price)) +
@@ -56,24 +52,24 @@ plot2 <- reactive({
         stat_bin2d(bin=25, color="white")
     }
   })
-    
-output$plot2 <- renderPlot({
-  plot2()
-    })
   
-#for clicking on the plot and select a region to show the data accordingly
-#for clicking the scatter plot (2nd tab)
-output$subsets <- renderPrint({
-  brushedPoints(diamonds, input$plot_brush, xvar = "carat", yvar = "price")
-})
-
-#for clicking the heatmap (3rd tab)
-output$subsets2 <- renderPrint({
-  brushedPoints(diamonds, input$plot_brush, xvar = "carat", yvar = "price")
-})
-
-#for the 4th tab:show data set that been used
-Dataset <- reactive({
+  output$plot2 <- renderPlot({
+    plot2()
+  })
+  
+  #for clicking on the plot and select a region to show the data accordingly
+  #for clicking the scatter plot (2nd tab)
+  output$subsets <- renderPrint({
+    brushedPoints(diamonds, input$plot_brush, xvar = "carat", yvar = "price")
+  })
+  
+  #for clicking the heatmap (3rd tab)
+  output$subsets2 <- renderPrint({
+    brushedPoints(diamonds, input$plot_brush, xvar = "carat", yvar = "price")
+  })
+  
+  #for the 4th tab:show data set that been used
+  Dataset <- reactive({
     if(input$carats){
       Data<-newData
     } else {
@@ -81,11 +77,56 @@ Dataset <- reactive({
     }
   })
   
-output$datasets <- renderDataTable(Dataset())
+  output$datasets <- renderDataTable(Dataset())
   
-#add download button for different tabs
-#download button for the scatter plot (tab 2) 
-output$downloadData1 <- downloadHandler(
+  # for 5th tab: linear regression
+  modelFit <- reactive({
+    if(input$selection){
+      lm(as.formula(paste("price"," ~ ",paste(input$variables,collapse="+"))), data = diamonds)
+    }else{
+      "Please select variables."
+    }
+  })
+  
+  output$fitting <- renderPrint({modelFit()})
+  
+  # plot for 6th tab: Classification Tree
+  treeFit <- reactive({
+    newData <- diamonds
+    newData$cut <- as.factor(newData$cut)
+    newData$color <- as.factor(newData$color)
+    newData$clarity <- as.factor(newData$clarity)
+    if(input$selection){
+      fit <- tree(as.formula(paste("price"," ~ ",paste(input$variables,collapse="+"))), data = newData)
+      plot(fit)
+      text(fit)
+    }else{
+      "Please select variables"
+    }
+  })
+  
+  output$tree <- renderPlot({
+    treeFit()
+  })
+  
+  # plot for 7th tab: PCA
+  PCs <- reactive({
+    newData <- diamonds
+    newData$cut <- as.numeric(as.factor(newData$cut))
+    newData$color <- as.numeric(as.factor(newData$color))
+    newData$clarity <- as.numeric(as.factor(newData$clarity))
+    if(input$selection){
+      prcomp(select(newData, input$variables), scale = TRUE, center=TRUE)
+    }else{"Please select variables"}
+  })
+  
+  output$PCA <- renderPrint({
+    PCs()
+  })
+  
+  #add download button for different tabs
+  #download button for the scatter plot (tab 2) 
+  output$downloadData1 <- downloadHandler(
     filename = function(){
       paste("Relationship between carat and price-Scatter Plot", ".png", sep="")
     }, 
@@ -94,8 +135,8 @@ output$downloadData1 <- downloadHandler(
       
     })
   
-#download button for the heatmap (tab 3)
-output$downloadData2 <- downloadHandler(
+  #download button for the heatmap (tab 3)
+  output$downloadData2 <- downloadHandler(
     filename = function(){
       paste("Relationship between carat and price-Heatmap", ".png", sep="")
     }, 
@@ -103,8 +144,8 @@ output$downloadData2 <- downloadHandler(
     {ggsave(file,plot2())
     })
   
-#download button for the dataset been used (tab 3)
-output$downloadData3 <- downloadHandler(
+  #download button for the dataset been used (tab 3)
+  output$downloadData3 <- downloadHandler(
     filename = function(){
       paste("Diamonds Dataset", ".csv", sep="")
     }, 
--- ui.R
+++ ui.R
@@ -1,6 +1,5 @@
 library(shiny)
 
-
 # Define the overall UI
 shinyUI(
   fluidPage(    
@@ -15,7 +14,14 @@ shinyUI(
         conditionalPanel(
           condition="input.carats == '1'",
           checkboxInput("cuts", h5("Color Code Different Cut"))
+        ),
+        br(),
+        checkboxInput("selection", h5("Select variables to be used in supervised learning and unsupervised learning", style = "color:red;")),
+        conditionalPanel(
+          condition="input.selection == '1'",
+          checkboxGroupInput("variables", "Varibles to be used:", c("carat", "cut", "color", "clarity", "depth"))
         )
+        
       ),
       
       mainPanel(
@@ -26,20 +32,26 @@ shinyUI(
                              uiOutput("link"), 
                              #include a math symbol that need to use mathJax
                              withMathJax(helpText("May use transformations of price such as \\(\\sqrt{y}\\) to find linear relationship between carat and price")) 
-                             ),
+                    ),
                     tabPanel("Scatter Plot", 
                              plotOutput("plot1", brush = "plot_brush"), #option for select a region on the plot 
                              downloadButton("downloadData1","Download"), 
                              verbatimTextOutput("subsets") #when select a region, show the data accordingly
-                             ),
+                    ),
                     tabPanel("Heatmap", 
                              plotOutput("plot2", brush = "plot_brush"), #option for select a region on the plot
                              downloadButton("downloadData2","Download"), 
                              verbatimTextOutput("subsets2") #when select a region, show the data accordingly
-                             ),
+                    ),
                     tabPanel("Dataset", 
                              dataTableOutput("datasets"), 
-                             downloadButton("downloadData3","Download"))
+                             downloadButton("downloadData3","Download")),
+                    tabPanel("Regression", 
+                             verbatimTextOutput("fitting")),
+                    tabPanel("Classification Tree", 
+                             plotOutput("tree")),
+                    tabPanel("PCA", 
+                             verbatimTextOutput("PCA"))
         )
       )  
     )
